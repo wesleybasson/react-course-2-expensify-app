8b39a4a32632625d0a06d0a03f97f85fa2fcecd9
Wesley Basson <basso.we@gmail.com>
2018-12-11 17:04:48 +0200

    Add Facebook login

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { startLogin } from '../actions/auth';
+import { startGoogleLogin, startFacebookLogin } from '../actions/auth';
 
-export const LoginPage = ({ startLogin }) => (
+export const LoginPage = ({ startGoogleLogin, startFacebookLogin }) => (
     <div className="box-layout">
         <div className="box-layout__box">
             <h1 className="box-layout__title">Expensify</h1>
             <p>It's time to get your expenses under control.</p>
-            <button className="button" onClick={startLogin}>Login with Google</button>
+            <button className="button button__login button__bottommargin button--google" onClick={startGoogleLogin}>Login with Google</button>
+            <button className="button button__login button--facebook" onClick={startFacebookLogin}>Login with Facebook</button>
         </div>
     </div>
 );
 
 const mapDispatchToProps = (dispatch) => ({
-    startLogin: () => dispatch(startLogin())
+    startGoogleLogin: () => dispatch(startGoogleLogin()),
+    startFacebookLogin: () => dispatch(startFacebookLogin())
 });
 
 export default connect(undefined, mapDispatchToProps)(LoginPage);
--- src/tests/components/LoginPage.test.js
+++ src/tests/components/LoginPage.test.js
@@ -2,18 +2,24 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { LoginPage } from '../../components/LoginPage';
 
-let wrapper, startLogin;
+let wrapper, startGoogleLogin, startFacebookLogin;
 
 beforeEach(() => {
-    startLogin = jest.fn();
-    wrapper = shallow(<LoginPage startLogin={startLogin} />);
+    startGoogleLogin = jest.fn();
+    startFacebookLogin = jest.fn();
+    wrapper = shallow(<LoginPage startGoogleLogin={startGoogleLogin} startFacebookLogin={startFacebookLogin} />);
 });
 
 test('should render LoginPage correctly', () => {
     expect(wrapper).toMatchSnapshot();
 });
 
-test('should call startLogin on button click', () => {
-    wrapper.find('button').simulate('click');
-    expect(startLogin).toHaveBeenCalled();
+test('should call startGoogleLogin on button click', () => {
+    wrapper.find('button').at(0).simulate('click');
+    expect(startGoogleLogin).toHaveBeenCalled();
+});
+
+test('should call startFacebookLogin on button click', () => {
+    wrapper.find('button').at(1).simulate('click');
+    expect(startFacebookLogin).toHaveBeenCalled();
 });
